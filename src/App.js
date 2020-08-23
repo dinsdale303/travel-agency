66daf92c2b5913ac368d8d1b8d9969f4eef77ff9
Piotr Stefanowicz <piotrekstefanowicz@gmail.com>
2020-08-23 10:52:55 +0200

    Add routing to Trips and Countries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import PropTypes from 'prop-types';
 
 import MainLayout from './components/layout/MainLayout/MainLayout';
@@ -8,54 +9,67 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 // TODO - import other views
+import Countries from './components/views/Countries/CountriesContainer';
+import Regions from './components/views/Regions/RegionsContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
-
+import CountryContainer from './components/views/Country/CountryContainer';
+import TripContainer from './components/views/Trip/TripContainer';
 import parseTrips from './utils/parseTrips';
-import {setMultipleStates} from './redux/globalRedux';
+import { setMultipleStates } from './redux/globalRedux';
+
+import styles from './styles/global.scss';
 
 class App extends React.Component {
   static propTypes = {
     trips: PropTypes.array,
     setStates: PropTypes.func,
-  }
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // parse trips when App is first created
     parseTrips(this.props.trips, this.props.setStates);
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps.trips != this.props.trips){
+  componentDidUpdate(prevProps) {
+    if (prevProps.trips != this.props.trips) {
       // parse trips again if they changed
       parseTrips(this.props.trips, this.props.setStates);
     }
   }
 
-  render(){
+  render() {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/trips' component={Trips} />
-            {/* TODO - add more routes for other views */}
-            <Route exact path='/info' component={Info} />
-            <Route path='*' component={NotFound} />
-          </Switch>
+          <AnimatedSwitch
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+            className={styles.switchWrapper}
+          >
+            <Route exact path="/" component={Home} />
+            <Route exact path="/trips" component={Trips} />
+            <Route exact path="/countries" component={Countries} />
+            <Route exact path="/regions" component={Regions} />
+            <Route exact path="/info" component={Info} />
+            <Route exact path="/country/:id" component={CountryContainer} />
+            <Route exact path="/trip/:id" component={TripContainer} />
+            <Route path="*" component={NotFound} />
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   trips: state.trips,
 });
 
-const mapDispatchToProps = dispatch => ({
-  setStates: newState => dispatch(setMultipleStates(newState)),
+const mapDispatchToProps = (dispatch) => ({
+  setStates: (newState) => dispatch(setMultipleStates(newState)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,16 +1,26 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import TripListOptions from './TripListOptions';
-import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import { getAllTags } from '../../../redux/tagsRedux';
+import {
+  getAllFilters,
+  changeSearchPhrase,
+  addTag,
+  removeTag,
+  changeDuration,
+} from '../../../redux/filtersRedux';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   tags: getAllTags(state),
   filters: getAllFilters(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+const mapDispatchToProps = (dispatch) => ({
+  changeSearchPhrase: (phrase) => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+  addTag: (tag) => dispatch(addTag(tag)),
+  removeTag: (tag) => dispatch(removeTag(tag)),
+  changeDuration: ({ type, value }) =>
+    dispatch(changeDuration({ type, value })),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
